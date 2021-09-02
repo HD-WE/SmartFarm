631e7b45f44dedfa5998e67ebab8bc80e6483317
kimkyung1234 <71820026+kimkyung1234@users.noreply.github.com>
2021-09-02 18:06:45 +0900

    Add motor

---
+++ smartfarm/motor.ino
@@ -0,0 +1,25 @@
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop() 
+{
+  if(Serial.available())
+  {
+    String str = Serial.readString();
+
+    if(str == "ON")
+    {
+      digitalWrite(13,HIGH);
+      Serial.println("LED on");
+    }
+
+    else if(str == "OFF")
+    {
+      digitalWrite(13,LOW);
+      Serial.prinln("LED off");
+    }
+  }
+}
