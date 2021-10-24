5790a3efaadb69c7640d959ee355c50ae9d02c43
MoonSeoJun <msj00130@gmail.com>
2021-10-24 16:02:46 +0900

    Modify classify functions

--- server.py
+++ server.py
@@ -6,29 +6,35 @@ import datetime
 
 app = Flask(__name__)
 
-get_enviorn = "SELECT temp, humi FROM farm ORDER BY date DESC LIMIT 1"
-get_all = "SELECT * from farm"
+get_water = "SELECT temp, humi FROM farm WHERE type=\"water\" ORDER BY date DESC LIMIT 1"
+get_ground = "SELECT temp, humi FROM farm WHERE type=\"ground\" ORDER BY date DESC LIMIT 1"
 add_environ = "INSERT INTO farm (temp, humi, type, date) VALUES (?, ?, ?, ?)"
 
 
-@app.route('/environ', methods=['POST', 'GET'])
-def environ():
+@app.route('/environ', methods=['POST'])
+def save_environ():
     c = conn.cursor()
-    if request.method == 'GET':
-        result = c.execute(get_enviorn).fetchall()
+
+    value = request.json
+
+    now = datetime.datetime.now()
+
+    c.execute(add_environ, (value["temp"], value["humi"], value["type"], str(now)))
+    conn.commit()
+
+    return "succes!"
+
+@app.route('/environ/<type>')
+def get_environ(type):
+    c = conn.cursor()
+    if type == "water":
+        result = c.execute(get_water).fetchall()
         return jsonify({"temp" : result[0][0],
                     "humi" : result[0][1]})
-    elif request.method == 'POST':
-        value = request.json
-
-        print(value["temp"])
-
-        now = datetime.datetime.now()
-
-        c.execute(add_environ, (value["temp"], value["humi"], value["type"], str(now)))
-        conn.commit()
-
-        return "succes!"
+    elif type == "ground":
+        result = c.execute(get_ground).fetchall()
+        return jsonify({"temp" : result[0][0],
+                    "humi" : result[0][1]})
         
 if __name__ == "__main__":
     conn = sqlite3.connect('farm.db', check_same_thread=False)
